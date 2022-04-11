bbc2d0d0552bb6aaed3050637672c71dd7dfb767
Jude0124 <legagain@gmail.com>
2022-04-11 23:04:51 +0900

    GUKBIT_Base_setting

---
+++ src/main/java/com/gukbit/GukbitApplication.java
@@ -0,0 +1,13 @@
+package com.gukbit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GukbitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GukbitApplication.class, args);
+	}
+
+}
