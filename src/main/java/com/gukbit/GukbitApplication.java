5589b6b88ca6b16881874a27955f5b3444d744bc
Sam <vkdnsxkdlrj@gmail.com>
2022-04-13 23:34:32 +0900

    04-13

--- src/main/java/com/gukbit/GukbitApplication.java
+++ src/main/java/com/gukbit/GukbitApplication.java
@@ -13,27 +13,27 @@ import java.util.stream.IntStream;
 @SpringBootApplication
 public class GukbitApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(GukbitApplication.class, args);
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(GukbitApplication.class, args);
+  }
 
-    @Bean
-    public CommandLineRunner runner(BoardRepository boardRepository) { //3. 명령 실행
-        return (args) -> {
-            IntStream.rangeClosed(1, 100).forEach(index ->
-                    boardRepository.save(Board.builder()
-                            .no(index)
-                            .author("이순신")
-                            .date(LocalDateTime.now())
-                            .view(2222)
-                            .title("국비학원 출신 10년차 개발자의 개인적인 의견입니다.")
-                            .content("")
-                            .academyCode("[멀티캠퍼스]")
-                            .courseId("[웹개발 풀스택 과정]")
-                            .visible(true)
-                            .recommend(0)
-                            .build())
-            );
-        };
-    }
+  @Bean
+  public CommandLineRunner runner(BoardRepository boardRepository) { //3. 명령 실행
+    return (args) -> {
+      IntStream.rangeClosed(1, 100).forEach(index ->
+          boardRepository.save(Board.builder()
+              .no(index)
+              .author("이순신")
+              .date(LocalDateTime.now())
+              .view(2222)
+              .title("국비학원 출신 10년차 개발자의 개인적인 의견입니다.")
+              .content("")
+              .academyCode("[멀티캠퍼스]")
+              .courseId("[웹개발 풀스택 과정]")
+              .visible(true)
+              .recommend(0)
+              .build())
+      );
+    };
+  }
 }
