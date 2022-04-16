8a4a109d3a4ad1ae5a50d04498b040b2637b38b8
Sam <vkdnsxkdlrj@gmail.com>
2022-04-16 20:40:33 +0900

    회원가입 폼 정보 jpa를 사용해 db 저장

---
+++ src/main/java/com/gukbit/domain/User.java
@@ -0,0 +1,43 @@
+package com.gukbit.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Builder
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "user")
+public class User {
+
+  @Id
+  @Column(name = "user_idx")
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long user_idx;
+  @Column
+  private String user_id;
+  @Column
+  private String password;
+  @Column(unique = true)
+  private String email;
+  @Column
+  private String tel;
+
+  @Builder
+  public User(Long user_idx, String userId, String password, String email, String tel) {
+    this.user_idx = user_idx;
+    this.user_id = userId;
+    this.password = password;
+    this.email = email;
+    this.tel = tel;
+  }
+}
--- src/main/java/com/gukbit/repository/MemberRepository.java
+++
@@ -1,8 +0,0 @@
-package com.gukbit.repository;
-
-import com.gukbit.model.MemberModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberRepository extends JpaRepository {
-  public int setInsert(MemberModel memberVO) throws Exception;
-}
