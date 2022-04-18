2e84946aafb07f283bc80958a36ecb52922b9d7b
m16khb <m16khb@gmail.com>
2022-04-18 22:38:02 +0900

    임포트 없어진거 지우기

--- src/main/java/com/gukbit/controller/IndexController.java
+++ src/main/java/com/gukbit/controller/IndexController.java
@@ -8,8 +8,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.SessionAttribute;
-import java.util.ArrayList;
-import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
