370243b247eceee3647505611ba4f025373ff313
Jude0124 <legagain@gmail.com>
2022-04-18 23:45:52 +0900

    .

--- src/main/java/com/gukbit/GukbitApplication.java
+++ src/main/java/com/gukbit/GukbitApplication.java
@@ -2,13 +2,10 @@ package com.gukbit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-<<<<<<< HEAD
-=======
 import org.springframework.context.annotation.Bean;
 import java.time.LocalDateTime;
 import java.util.Map;
 import java.util.stream.LongStream;
->>>>>>> ee019b14c266127f624f28180d3775b07a2c6437
 
 @SpringBootApplication
 public class GukbitApplication {
@@ -17,10 +14,6 @@ public class GukbitApplication {
     SpringApplication.run(GukbitApplication.class, args);
   }
 
-<<<<<<< HEAD
-
-=======
->>>>>>> ee019b14c266127f624f28180d3775b07a2c6437
 //  @Bean
 //  public CommandLineRunner runner(AcademyRepository academyRepository) { //3. 명령 실행
 //    Map<String, AcademyData> map = AcademyList.academylist();
@@ -38,27 +31,6 @@ public class GukbitApplication {
 //    };
 //  }
 //
-<<<<<<< HEAD
-//    @Bean
-//    public CommandLineRunner runner2(CourseRepository courseRepository) { //3. 명령 실행
-//      Map<Integer, CourseData> map = CourseList.courselist();
-//      return (args) -> {
-//        map.forEach((key, value) ->
-//                courseRepository.save(Course.builder()
-//                        .academy_code(value.getAcademyId())
-//                        .id(value.getCourseId())
-//                        .session(Integer.parseInt(value.getSession()))
-//                        .field_m(value.getField_m())
-//                        .field_s(value.getField_s())
-//                        .d_field_ss(value.getD_field_ss())
-//                        .name(value.getTitle())
-//                        .start(value.getStart())
-//                        .end(value.getEnd())
-//                        .build())
-//        );
-//      };
-//    }
-=======
 //  @Bean
 //  public CommandLineRunner runner2(CourseRepository courseRepository) { //3. 명령 실행
 //    Map<Integer, CourseData> map = CourseList.courselist();
@@ -78,24 +50,16 @@ public class GukbitApplication {
 //      );
 //    };
 //  }
->>>>>>> ee019b14c266127f624f28180d3775b07a2c6437
 //
 //  @Bean
 //  public CommandLineRunner runner3(BoardRepository boardRepository) { //3. 명령 실행
 //    return (args) -> {
 //      LongStream.rangeClosed(1, 100).forEach(index ->
 //          boardRepository.save(Board.builder()
-<<<<<<< HEAD
-//              .bid((long) index)
-//              .author("이순신")
-//              .date(String.valueOf(LocalDateTime.now()))
-//              .view(2222)
-=======
 //              .bid(index)
 //              .author("이순신")
 //              .date(LocalDateTime.now().toString())
 //              .view((int)index)
->>>>>>> ee019b14c266127f624f28180d3775b07a2c6437
 //              .title("국비학원 출신 10년차 개발자의 개인적인 의견입니다.")
 //              .content("")
 //              .b_academy_code("[멀티캠퍼스]")
@@ -106,8 +70,4 @@ public class GukbitApplication {
 //      );
 //    };
 //  }
-<<<<<<< HEAD
-
-=======
->>>>>>> ee019b14c266127f624f28180d3775b07a2c6437
 }
