647e38425f10b31e29da02794a860a54d2f7d07d
Perhona <perhona@kakao.com>
2022-04-19 13:05:00 +0900

    학원리뷰 페이지 selectbox 과정 연동&userid가져오기 1차 구현

--- src/main/java/com/gukbit/service/RateService.java
+++ src/main/java/com/gukbit/service/RateService.java
@@ -1,9 +1,11 @@
 package com.gukbit.service;
 
 import com.gukbit.domain.Academy;
+import com.gukbit.domain.Course;
 import com.gukbit.dto.AcademyDto;
 import com.gukbit.dto.RateDto;
 import com.gukbit.repository.AcademyRepository;
+import com.gukbit.repository.CourseRepository;
 import com.gukbit.repository.RateRepository;
 import java.util.ArrayList;
 import java.util.List;
@@ -14,14 +16,23 @@ import org.springframework.stereotype.Service;
 public class RateService {
 
   private RateRepository rateRepository;
+  private CourseRepository courseRepository;
 
-  public RateService(RateRepository rateRepository) {
+  public RateService(RateRepository rateRepository, CourseRepository courseRepository) {
     this.rateRepository = rateRepository;
+    this.courseRepository = courseRepository;
   }
 
   @Transactional
   public void saveReview(RateDto rateDto) {
     System.out.println("save review 페이지 "+ rateDto);
     rateRepository.save(rateDto.toEntity());
+  }
+
+  public List<Course> getCoursesByAcademyCode(String academyCode) {
+    System.out.println("rateService 도착" + academyCode);
+    List<Course> courseListForAcademy = courseRepository.findByAcademycode(academyCode);
+    System.out.println("RateService: courseListForAcademy"+courseListForAcademy);
+    return courseListForAcademy;
   }
 }
