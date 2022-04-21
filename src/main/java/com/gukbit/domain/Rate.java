8e72bf6ddb53d050e54e997097b9e87f1fc8e176
Jude0124 <legagain@gmail.com>
2022-04-21 16:40:04 +0900

    평점 리뷰 페이지 1차

--- src/main/java/com/gukbit/domain/Rate.java
+++ src/main/java/com/gukbit/domain/Rate.java
@@ -2,13 +2,11 @@ package com.gukbit.domain;
 
 import lombok.*;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 
-@Getter @Setter
+@Getter
+@Setter
 @Entity
 @Table(name = "rate")
 @Builder
@@ -16,30 +14,36 @@ import javax.persistence.Table;
 @AllArgsConstructor
 public class Rate {
 
-  @Id
-  @Column(name = "rid")
-  private String rid;
-  @Column(name = "c_cid")
-  private String cCid;
-  @Column(name = "user_id")
-  private String userId;
-  @Column(name = "one_statement")
-  private String oneStatement;
-  @Column(name = "lecturers_eval")
-  private Double lecturersEval;
-  @Column(name = "curriculum_eval")
-  private Double curriculumEval;
-  @Column(name = "employment_eval")
-  private Double employmentEval;
-  @Column(name = "culture_eval")
-  private Double cultureEval;
-  @Column(name = "facility_eval")
-  private Double facilityEval;
-  @Column(name = "advantage")
-  private String advantage;
-  @Column(name = "disadvantage")
-  private String disadvantage;
+    @Id
+    @Column(name = "rid")
+    private String rid;
+    @Column(name = "c_cid")
+    private String cCid;
+    @Column(name = "user_id")
+    private String userId;
+    @Column(name = "one_statement")
+    private String oneStatement;
+    @Column(name = "lecturers_eval")
+    private Double lecturersEval;
+    @Column(name = "curriculum_eval")
+    private Double curriculumEval;
+    @Column(name = "employment_eval")
+    private Double employmentEval;
+    @Column(name = "culture_eval")
+    private Double cultureEval;
+    @Column(name = "facility_eval")
+    private Double facilityEval;
+    @Column(name = "advantage")
+    private String advantage;
+    @Column(name = "disadvantage")
+    private String disadvantage;
 
-  public Rate() {
-  }
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "c_cid", referencedColumnName = "cid", insertable = false, updatable = false)
+    private Course course;
+
+    public Rate() {
+    }
+
+
 }
--- src/main/java/com/gukbit/repository/RateRepository.java
+++ src/main/java/com/gukbit/repository/RateRepository.java
@@ -2,11 +2,27 @@
 package com.gukbit.repository;
 
 import com.gukbit.domain.Rate;
-import com.gukbit.domain.User;
+import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface RateRepository extends JpaRepository<Rate, String> {
     Rate findByUserId(String userId);
+
     void deleteByUserId(String userId);
+
+    Rate findByRid(String rid);
+
+
+    @Query("select r from Rate r join fetch r.course  where r.cCid=:cCid")
+    List<Rate> findAllByCCid(@Param(value="cCid") String cCid);
+
+//    @Query("select AVG(lecturers_eval),AVG(curriculum_eval),AVG(employment_eval),AVG(culture_eval),AVG(facility_eval) " +
+//            "from (SELECT * FROM Rate r join fetch r.course  where r.c_cid = :c_cid)")
+//    List<Rate> findAllByCCid(@Param(value="c_cid") String c_cid);
+//
+
 }
 
