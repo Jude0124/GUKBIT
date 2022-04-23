0b1e523997dcbceb5204fefb9c1c6e91d776bd7e
Perhona <95855455+Perhona@users.noreply.github.com>
2022-04-23 23:27:46 +0900

    Revert "Feature/fix community alert"

--- src/main/java/com/gukbit/controller/CommunityController.java
+++ src/main/java/com/gukbit/controller/CommunityController.java
@@ -1,36 +1,41 @@
 package com.gukbit.controller;
 
 
+
+import com.gukbit.domain.AuthUserData;
+import com.gukbit.domain.Board;
+import com.gukbit.domain.User;
+import com.gukbit.etc.ReplyDto;
+
 import com.gukbit.domain.Academy;
 import com.gukbit.domain.AuthUserData;
 import com.gukbit.domain.Board;
 import com.gukbit.domain.Course;
+import com.gukbit.domain.Reply;
 import com.gukbit.domain.User;
 import com.gukbit.etc.ReplyDto;
-import com.gukbit.etc.Today;
 import com.gukbit.service.AcademyService;
 import com.gukbit.service.BoardService;
+import com.gukbit.repository.BoardRepository;
 import com.gukbit.service.CourseService;
 import com.gukbit.service.RateService;
 import com.gukbit.service.ReplyService;
+import com.gukbit.etc.Today;
+import com.gukbit.service.BoardService;
+import com.gukbit.service.ReplyService;
 import com.gukbit.service.UserService;
 import com.gukbit.session.SessionConst;
-import java.util.List;
-import java.util.Map;
+import java.util.ArrayList;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.SessionAttribute;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
 
 
 @Slf4j
@@ -76,7 +81,7 @@ public class CommunityController {
     public String alignByView(Pageable pageable, Model model) {
         Page<Board> p = boardService.alignByView(pageable);
         model.addAttribute("boardList", p);
-        return "view/communityboardView";
+        return "view/communityboard";
     }
 
 
--- src/main/java/com/gukbit/repository/BoardRepository.java
+++ src/main/java/com/gukbit/repository/BoardRepository.java
@@ -1,6 +1,7 @@
 package com.gukbit.repository;
 
 import com.gukbit.domain.Board;
+import com.gukbit.domain.Course;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,13 +10,14 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 
 @Repository
 public interface BoardRepository extends JpaRepository<Board, Integer> {
     Board findByAuthor(String author);
 
     Page<Board> findByBacademycode(String academyCode, Pageable pageable);
-
 
     @Modifying
     @Query("update Board a set a.view = a.view + 1 where a.bid = :id")
