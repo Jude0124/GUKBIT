0ab14f29cdce235323fb65fb31c2e0063ece1593
Sam <vkdnsxkdlrj@gmail.com>
2022-04-24 18:36:43 +0900

    import

--- src/main/java/com/gukbit/controller/AcademyController.java
+++ src/main/java/com/gukbit/controller/AcademyController.java
@@ -11,7 +11,6 @@ import com.gukbit.service.RateService;
 import com.gukbit.session.SessionConst;
 import java.util.ArrayList;
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.domain.Page;
@@ -19,8 +18,10 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.SessionAttribute;
 
 @Controller
