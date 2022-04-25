98e6a4ab4a4a4f495aceeff1cd51ef81573828c1
Perhona <perhona@kakao.com>
2022-04-25 14:58:36 +0900

    mypage alert => sweetalert2 1차 적용

--- src/main/resources/static/js/mypageValidation.js
+++ src/main/resources/static/js/mypageValidation.js
@@ -23,7 +23,8 @@
 // });
 
 $(document).ready(function () {
-	$('.userInfo').submit(function () {
+
+	$('.userInfo').submit(function (e) {
 		let pwCheck = 0;
 		let formCheck = 0;
 
@@ -45,42 +46,105 @@ $(document).ready(function () {
 		}
 
 		if (formCheck && pwCheck === 2) {
-			if (
-				confirm(
-					'비밀번호와 과정을 수정합니다. 정말 수정하시겠습니까? ※과정을 수정하시면 이전에 작성했던 평가와 별점이 삭제됩니다.',
-				)
-			) {
-				return true;
-			}
+			// if (
+			// 	confirm(
+			// 		'비밀번호와 과정을 수정합니다. 정말 수정하시겠습니까? ※과정을 수정하시면 이전에 작성했던 평가와 별점이 삭제됩니다.',
+			// 	)
+			// ) {
+			// 	return true;
+			// }
+			Swal.fire({
+				title: '비밀번호와 과정을 수정합니다.<br>정말 수정하시겠습니까?',
+				text: "※과정을 수정하시면 이전에 작성했던 평가와 별점이 삭제됩니다.",
+				icon: 'warning',
+				showCancelButton: true,
+				confirmButtonColor: '#0059ab',
+				cancelButtonColor: '#D5D5D5',
+				confirmButtonText: '확인',
+				cancelButtonText: '취소'
+			}).then((result) => {
+				if (result.isConfirmed) {
+					$(this).unbind();
+					$(this).submit();
+					return true;
+				}
+			})
+
 			return false;
 		} else if (formCheck && pwCheck === 3) {
-			if (
-				confirm(
-					'과정을 수정합니다. 정말 수정하시겠습니까? ※과정을 수정하시면 이전에 작성했던 평가와 별점이 삭제됩니다.',
-				)
-			) {
-				return true;
-			}
+			// if (
+			// 	confirm(
+			// 		'과정을 수정합니다. 정말 수정하시겠습니까? ※과정을 수정하시면 이전에 작성했던 평가와 별점이 삭제됩니다.',
+			// 	)
+			// ) {
+			// 	return true;
+			// }
+			Swal.fire({
+				title: '과정을 수정합니다.<br>정말 수정하시겠습니까?',
+				text: "※과정을 수정하시면 이전에 작성했던 평가와 별점이 삭제됩니다.",
+				icon: 'warning',
+				showCancelButton: true,
+				confirmButtonColor: '#0059ab',
+				cancelButtonColor: '#D5D5D5',
+				confirmButtonText: '확인',
+				cancelButtonText: '취소'
+			}).then((result) => {
+				if (result.isConfirmed) {
+					$(this).unbind();
+					$(this).submit();
+					return true;
+				}
+			})
 			return false;
 		}
 
 		if (pwCheck === 2) {
-			if (confirm('비밀번호를 수정하시겠습니까?')) {
-				return true;
-			}
+			// if (confirm('비밀번호를 수정하시겠습니까?')) {
+			// 	return true;
+			// }
+			Swal.fire({
+				title: '비밀번호를 수정하시겠습니까?',
+				icon: 'question',
+				showCancelButton: true,
+				confirmButtonColor: '#0059ab',
+				cancelButtonColor: '#D5D5D5',
+				confirmButtonText: '확인',
+				cancelButtonText: '취소'
+			}).then((result) => {
+				if (result.isConfirmed) {
+					$(this).unbind();
+					$(this).submit();
+					return true;
+				}
+			})
 		}
 
 		if (formCheck === 0 && pwCheck === 3) {
-			alert('수정된 항목이 없습니다.');
+			e.preventDefault();
+			// alert('수정된 항목이 없습니다.');
+			Swal.fire({
+				icon: 'info',
+				text: '수정된 항목이 없습니다',
+				confirmButtonColor: '#0059ab',
+				confirmButtonText: '확인'
+			})
+
 			return false;
 		}
 
 		if (pwCheck < 2) {
-			alert('비밀번호는 4자이상 16자 미만의 영문과 숫자가 가능합니다.');
+			// alert('비밀번호는 4자이상 16자 미만의 영문과 숫자가 가능합니다.');
+			e.preventDefault();
+			Swal.fire({
+				icon: 'warning',
+				html: '비밀번호는 4자 이상 16자 미만의<br>영문과 숫자 조합으로 입력해주세요.',
+				confirmButtonColor: '#0059ab',
+				confirmButtonText: '확인'
+			})
 			return false;
 		}
 		return false;
-	});
+	}); // mypage 유효성 검사 끝
 
 	/* 평가 수정/삭제 버튼 클릭 시 */
 	$('#authCourseRewrite').click(function (e) {
@@ -101,5 +165,8 @@ $(document).ready(function () {
 			return false;
 		}
 		alert('평가 수정/삭제 페이지로 이동합니다.');
-	});
+
+	});	// click이벤트 끝 
+
+
 });
