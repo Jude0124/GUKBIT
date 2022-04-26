23b43fa1c00994ebc646a89e276f9b37da582284
Jude0124 <legagain@gmail.com>
2022-04-26 15:00:23 +0900

    b_course_name => bCourseName

--- src/main/java/com/gukbit/domain/Board.java
+++ src/main/java/com/gukbit/domain/Board.java
@@ -1,5 +1,6 @@
 package com.gukbit.domain;
 
+import com.gukbit.controller.CommunityController;
 import lombok.*;
 
 import javax.persistence.*;
@@ -10,7 +11,7 @@ import javax.persistence.*;
 @Entity
 @Table
 @ToString
-public class Board{
+public class Board extends CommunityController {
 
     @Id
     @Column
@@ -38,8 +39,8 @@ public class Board{
     @Column
     private String b_course_id;
 
-    @Column
-    private String b_course_name;
+    @Column (name = "b_course_name")
+    private String bCourseName;
 
     @Column (insertable=false)
     private Boolean visible;
@@ -54,7 +55,7 @@ public class Board{
     @Builder
     public Board(Integer bid, String author, String date, Integer view, String title, String content,
         String b_academy_code, String b_course_id, String b_academy_name,
-        boolean visible, int recommend, String b_course_name) {
+        boolean visible, int recommend, String bCourseName) {
         this.bid = bid;
         this.author = author;
         this.date = date;
@@ -66,6 +67,6 @@ public class Board{
         this.bacademyname = b_academy_name;
         this.visible = visible;
         this.recommend = recommend;
-        this.b_course_name = b_course_name;
+        this.bCourseName = bCourseName;
     }
 }
