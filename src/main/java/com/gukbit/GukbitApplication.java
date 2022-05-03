9554c361d55dff49f003c32927572308a835c423
Sam <vkdnsxkdlrj@gmail.com>
2022-05-03 16:20:29 +0900

    일단 db에 longblob으로 단일 이미지 등록은 가능한 상황

--- src/main/java/com/gukbit/GukbitApplication.java
+++ src/main/java/com/gukbit/GukbitApplication.java
@@ -1,11 +1,7 @@
 package com.gukbit;
 
-import org.apache.tomcat.util.http.LegacyCookieProcessor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.server.WebServerFactoryCustomizer;
-import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 
@@ -16,6 +12,7 @@ public class GukbitApplication {
     public static void main(String[] args) {
         SpringApplication.run(GukbitApplication.class, args);
     }
+    
 }
 
 
