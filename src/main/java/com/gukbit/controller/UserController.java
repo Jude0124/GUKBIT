6ed6157b38194baca8bcd35818268a0c5d059cb4
Perhona <perhona@kakao.com>
2022-05-05 02:22:31 +0900

    네이버 OCR 1차 적용

--- src/main/java/com/gukbit/controller/UserController.java
+++ src/main/java/com/gukbit/controller/UserController.java
@@ -5,11 +5,23 @@ import com.gukbit.domain.User;
 import com.gukbit.etc.UpdateUserData;
 import com.gukbit.service.UserService;
 import com.gukbit.session.SessionConst;
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.UUID;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Controller;
@@ -17,6 +29,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 @RequestMapping("/user")
@@ -107,5 +120,102 @@ public class UserController {
     @Getter @Setter //여기서만 쓸 클래스
     public static class PwCheck {
         String password;
+    }
+
+    /* mypage OCR 사진 업로드 */
+    @PostMapping("/mypage/ocr")
+    public String testOcr(@RequestParam("ocrFile") MultipartFile ocrFile) throws IOException {
+        String apiURL = "https://aebb11c320dd4cfca45990eca440b43f.apigw.ntruss.com/custom/v1/15639/ff487b347776c3629bf3c0d5b8f4a98702e2fcd1740893ec1c61b6cd4c00eb33/infer";
+        String secretKey = "Vk51RlZ3eHlJQ0JkamhXbUlBbkR1QkFITm1WRWx6aGo=";
+        String imageFile = ocrFile.getOriginalFilename();
+        System.out.println(imageFile);
+
+        try {
+            URL url = new URL(apiURL);
+            HttpURLConnection con = (HttpURLConnection)url.openConnection();
+            con.setUseCaches(false);
+            con.setDoInput(true);
+            con.setDoOutput(true);
+            con.setReadTimeout(30000);
+            con.setRequestMethod("POST");
+            String boundary = "----" + UUID.randomUUID().toString().replaceAll("-", "");
+            con.setRequestProperty("Content-Type", "multipart/form-data; boundary=" + boundary);
+            con.setRequestProperty("X-OCR-SECRET", secretKey);
+
+            JSONObject json = new JSONObject();
+            json.put("version", "V2");
+            json.put("requestId", UUID.randomUUID().toString());
+            json.put("timestamp", System.currentTimeMillis());
+
+            JSONObject image = new JSONObject();
+            image.put("format", imageFile.substring(imageFile.lastIndexOf(".")+1));
+            System.out.println(imageFile.substring(imageFile.lastIndexOf(".")+1));
+            image.put("name", "demo");
+            JSONArray images = new JSONArray();
+            images.add(image);
+            json.put("images", images);
+            String postParams = json.toString();
+
+            con.connect();
+            DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+            long start = System.currentTimeMillis();
+            File file = new File(imageFile);
+            writeMultiPart(wr, postParams, file, boundary);
+            wr.close();
+
+            /* 서버 응답 */
+            int responseCode = con.getResponseCode();
+            BufferedReader br;
+            if (responseCode == 200) {
+                br = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            } else {
+                br = new BufferedReader(new InputStreamReader(con.getErrorStream()));
+            }
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+            while ((inputLine = br.readLine()) != null) {
+                response.append(inputLine);
+            }
+            br.close();
+
+            System.out.println(response);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        return "redirect:/";
+    }
+    private static void writeMultiPart(OutputStream out, String jsonMessage, File file, String boundary) throws
+        IOException {
+        StringBuilder sb = new StringBuilder();
+        sb.append("--").append(boundary).append("\r\n");
+        sb.append("Content-Disposition:form-data; name=\"message\"\r\n\r\n");
+        sb.append(jsonMessage);
+        sb.append("\r\n");
+
+        out.write(sb.toString().getBytes("UTF-8"));
+        out.flush();
+
+        if (file != null && file.isFile()) {
+            out.write(("--" + boundary + "\r\n").getBytes("UTF-8"));
+            StringBuilder fileString = new StringBuilder();
+            fileString
+                .append("Content-Disposition:form-data; name=\"file\"; filename=");
+            fileString.append("\"" + file.getName() + "\"\r\n");
+            fileString.append("Content-Type: application/octet-stream\r\n\r\n");
+            out.write(fileString.toString().getBytes("UTF-8"));
+            out.flush();
+
+            try (FileInputStream fis = new FileInputStream(file)) {
+                byte[] buffer = new byte[8192];
+                int count;
+                while ((count = fis.read(buffer)) != -1) {
+                    out.write(buffer, 0, count);
+                }
+                out.write("\r\n".getBytes());
+            }
+
+            out.write(("--" + boundary + "--\r\n").getBytes("UTF-8"));
+        }
+        out.flush();
     }
 }
