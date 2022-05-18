c508641749f56bc5633d58b4efff4b1bc1490cfd
YangHun Jung <narwhal1123@gmail.com>
2022-05-18 09:32:45 +0900

    2nd_draft

--- src/main/java/com/gukbit/service/ChatService.java
+++ src/main/java/com/gukbit/service/ChatService.java
@@ -35,14 +35,6 @@ public class ChatService {
     // 채팅방 목록 출력
     public List<String> getMyChatroomList(String userId) {
         // 자기가 채팅한 기록이 있는 채팅방 리스트를 나오게...
-        return chatRepository.findByUserId(userId); // 아이디로 검색한 결과를 학원코드 distinct로 출력 - 채팅 한적 없으면 null -> if문으로 걸러야될듯
+        return chatRepository.findByUserId(userId); // 아이디로 검색한 결과를 학원코드 distinct로 출력
     }
-
-    // 채팅방 검색 기능
-    public void searchChatRoom(String keyword) {
-        // 학원명 검색을 해서(키워드검색) -> 학원 게시판 모달 처럼 학원 리스트를 띄우고 -> 결과를 클릭하면 채팅방 입장하도록
-        List<AcademyDto> academyDtoList = academyService.searchAcademy(keyword);
-    }
-
-
 }
