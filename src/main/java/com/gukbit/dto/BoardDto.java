762204c270526ff8f99d3dae5fe18edbf99da696
m16khb <m16khb@gmail.com>
2022-05-21 17:41:10 +0900

    시간수정

--- src/main/java/com/gukbit/dto/BoardDto.java
+++ src/main/java/com/gukbit/dto/BoardDto.java
@@ -2,6 +2,8 @@ package com.gukbit.dto;
 
 import com.gukbit.domain.Board;
 import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -25,7 +27,8 @@ public class BoardDto {
   private Integer recommend;
 
   public void setDateNow() {
-    this.date = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+    this.date = ZonedDateTime.now(ZoneId.of("Asia/Seoul")).format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+    //this.date = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
   }
 
   // Dto에서 필요한 부분을 빌더패턴을 통해 entity로 만드는 역할
